c7c81f72666d6ef97e2d466dfe0cd583c68476ce
Razvan Predescu <rzvdaniel@yahoo.com>
2016-09-11 11:06:47 +0100

    Initial commit.

---
+++ Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dotnet.Core.Simple.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
